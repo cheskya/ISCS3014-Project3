d6de64cfdf851f85a050108de4d77b71584cd5e1
Arthur Lluisma <ajclluisma@gmail.com>
2025-03-09 20:58:08 +0800

    Add death animation for enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var moving = true
 var speed = 130
 var health = 1
 
@@ -9,15 +10,20 @@ func _ready() -> void:
 	add_to_group("enemies")
 
 func _physics_process(delta: float) -> void:
-	translate(Vector2.DOWN * speed * delta)
+	if moving:
+		translate(Vector2.DOWN * speed * delta)
 
 func _on_detection_area_entered(area: Area2D) -> void:
+	health -= 1
+	
+	if health == 0:
+		moving = false
+		animated_sprite.play("death")
+		await animated_sprite.animation_finished
+		self.queue_free()
+		return
+		
 	var tween = get_tree().create_tween()
 	tween.tween_property(animated_sprite, "modulate", Color(255, 0, 0, 0.5), 0.2)
 	tween.tween_property(animated_sprite, "modulate", Color(255, 255, 255, 0.5), 0.2)
 	tween.tween_property(animated_sprite, "modulate", Color(1, 1, 1, 1), 0.1)
-	
-	if health == 0:
-		print("enemy dieded")
-	else:
-		health -= 1
