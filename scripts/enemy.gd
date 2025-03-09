71d7419809002e1d348730deb0073a084826ab98
Arthur Lluisma <ajclluisma@gmail.com>
2025-03-09 19:46:46 +0800

    Implement enemy health functionality

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
 var speed = 130
+var health = 1
 
 func _ready() -> void:
 	add_to_group("enemies")
 
 func _physics_process(delta: float) -> void:
 	translate(Vector2.DOWN * speed * delta)
+
+func _on_detection_area_entered(area: Area2D) -> void:
+	if health == 0:
+		print("enemy dieded")
+	else:
+		health -= 1
