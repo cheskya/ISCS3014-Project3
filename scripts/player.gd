999c511ac28b853035dae8d3533099438d21c855
Arthur Lluisma <ajclluisma@gmail.com>
2025-03-09 13:38:11 +0800

    Add basic player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,18 @@ extends CharacterBody2D
 
 @export var speed = 110
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	var move = Input.get_vector("left", "right", "up", "down")
+	
+	if move[0] == 0:
+		animated_sprite.play("idle")
+	elif move[0] > 0:
+		animated_sprite.play("move_right")
+	elif move[0] < 0:
+		animated_sprite.play("move_left")
+	
 	if move:
 		velocity = move * speed
 	else:
