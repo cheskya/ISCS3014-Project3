ed726cbaa6328ef268922021bb0119cf0fdcfae4
Chesca <fdjr401@gmail.com>
2025-03-11 23:38:55 +0800

    Add shoot timer to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
+@export var player_bullet: PackedScene = preload("res://scenes/playerbullet.tscn")
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var player_detection = $PlayerDetection
 @onready var hit_allow_timer = $HitAllow
+@onready var shoot_allow_timer = $ShootAllow
 
 var moving = true
 var hit_allow = true
+var shoot_allow = true
 
 var speed = 280
 var health = 3
@@ -50,6 +53,12 @@ func player_hit(enemy: Area2D, is_enemy_func: bool):
 	tween.tween_property(animated_sprite, "modulate", Color(255, 255, 255, 0.5), 0.3)
 	tween.tween_property(animated_sprite, "modulate", Color(1, 1, 1, 1), 0.1)
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("shoot") and shoot_allow:
+		print("shoot!")
+		shoot_allow = false
+		shoot_allow_timer.start()
+
 func _physics_process(delta: float) -> void:
 	if moving:
 		var viewport_size = get_viewport().size
@@ -78,6 +87,7 @@ func _physics_process(delta: float) -> void:
 			velocity = move * speed
 		else:
 			velocity = Vector2.ZERO
+		
 		move_and_slide()
 
 # Modify this function to accomodate bullets
@@ -90,3 +100,7 @@ func _on_hit_allow_timeout() -> void:
 		if is_instance_valid(enemy) and enemy.hit_allow and player_detection.overlaps_area(enemy):
 			hit_allow = false
 			player_hit(enemy, false)
+
+func _on_shoot_allow_timeout() -> void:
+	shoot_allow = true
+	pass # Replace with function body.
